d8553e810cbec61569133639572517ee5f754ea5
Estrol <estrol@entrosbot.xyz>
2020-12-10 08:48:43 +0700

    Added Utility project.

---
+++ Estrol.X3Jam.Log/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Estrol.X3Jam.Utility {
+    public static class Log {
+        public static void Write(string content) {
+            OutputToConsole(content);
+        }
+
+        public static void Write(string content, params object[] objects) {
+            content = string.Format(content, objects);
+
+            OutputToConsole(content);
+        }
+
+        private static void OutputToConsole(string content) {
+            string time = DateTime.Now.ToString("HH:mm:ss:ff");
+
+            Console.WriteLine("[{0}] {1}", time, content);
+        }
+    }
+}
